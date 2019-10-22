7ad9016ee3f9d2d9302b1ca4b298006f36aa89f4
FluentB <236718094@qq.com>
2019-10-22 12:06:49 +0800

    create table

---
+++ test/test.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `admin_user` (
+  `id` int(3) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `email` varchar(32) DEFAULT NULL,
+  `author` int(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+ 
+INSERT INTO `admin_user` VALUES ('1', 'admin', 'admin', 'admin@qq.com', '1');
