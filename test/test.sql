186bc0626560aad39d738ecbcb1000af591ceaea
FluentB <236718094@qq.com>
2019-11-13 01:51:50 +0800

    add char_user_ref

--- test/test.sql
+++ test/test.sql
@@ -1,31 +1,39 @@
 
 CREATE TABLE `admin_user` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(32) NOT NULL,
-  `password` varchar(32) NOT NULL,
- `email` varchar(32) NOT NULL,
-  `time` datetime DEFAULT NULL,
-  `encryptkey` varchar(32) ,
-  PRIMARY KEY (`id`)
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `username` varchar(32) NOT NULL,
+    `password` varchar(32) NOT NULL,
+    `email` varchar(32) NOT NULL,
+    `time` datetime DEFAULT NULL,
+    `encryptkey` varchar(32) ,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
  
 INSERT INTO `admin_user` VALUES ('1', 'admin', 'admin', 'admin@qq.com',NULL,NULL);
 
 
 CREATE TABLE `friend` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `userid1` int(11) NOT NULL,
-  `userid2` int(11) NOT NULL,
-  `time` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `userid1` int(11) NOT NULL,
+    `userid2` int(11) NOT NULL,
+    `time` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
  
 
 CREATE TABLE `chat` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `userid` int(11) NOT NULL,
-  `chatname` varchar(32) NOT NULL,
-  `create_time` datetime DEFAULT NULL,
-  `last_update_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `userid` int(11) NOT NULL,
+    `chatname` varchar(32) NOT NULL,
+    `create_time` datetime DEFAULT NULL,
+    `last_update_time` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `chat_user_ref` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `userid` int(11) NOT NULL,
+    `chatid` varchar(32) NOT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
