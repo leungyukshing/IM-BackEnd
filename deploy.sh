c4a40d67bab0454f8610cd0b4046c6623d1ba90f
leungyukshing <jacky14.liang@gmail.com>
2019-12-24 00:10:29 +0800

    fix docker problem

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi $(docker images -q)
 docker build -t test -f docker/Dockerfile .
-docker stop test
 docker run --rm -it -d -p 3004:8080 test
