f5584ea57f2cbc37b275be7d8d5b30b531be7113
FluentB <236718094@qq.com>
2019-12-24 13:19:44 +0800

    ci test

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker rmi -f $(docker images -q)
-docker build -t test -f docker/Dockerfile .
+docker build -t test/gotest -f docker/Dockerfile .
 docker run --rm -it -d -p 3004:8080 test
