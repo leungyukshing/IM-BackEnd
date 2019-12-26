0d6bbff41c9c9067ed4029f2aa46924eeb674abf
leungyukshing <jacky14.liang@gmail.com>
2019-12-26 14:56:23 +0800

    fix online db url

--- database/database.go
+++ database/database.go
@@ -3,6 +3,7 @@ package database
 import (
 	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 	"github.com/jinzhu/gorm"
 )
 
@@ -18,6 +19,7 @@ var Server *gorm.DB
 
 func InitMySQL() {
 	dsn := user + ":" + pass + "@tcp(" + url + ":" + port + ")/" + dbname + "?charset=utf8" + "&parseTime=True&loc=Local"
+	logs.Info("dsn: %v", dsn)
 	Server, err := gorm.Open("mysql", dsn)
 	if err == nil {
 		fmt.Println("open db success")
