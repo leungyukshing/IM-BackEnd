0b95c55d5756c2a39ea310572b253dc58d393c67
leungyukshing <jacky14.liang@gmail.com>
2020-02-04 21:29:41 +0800

    fix: fix docker port

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker stop $(docker ps -aq)
 docker build -t test -f Dockerfile .
-docker run --rm -it -d --net=host test /bin/bash # -c 'go run /go/src/github.com/backend/main.go'
+docker run --rm -it -d -p 8080:8080 --net=host test /bin/bash # -c 'go run /go/src/github.com/backend/main.go'
