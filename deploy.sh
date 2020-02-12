e5bccd33528c503be890d9d268a8c543ee9c3024
leungyukshing <jacky14.liang@gmail.com>
2020-02-12 20:57:24 +0800

    fix start file

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ docker rmi -f test:latest
 docker build -t test -f Dockerfile .
 docker run --rm -it -d -p 8080:8080 --net=host test
 docker cp /root/bee $(docker ps -q):/go/src/github.com/backend
-docker exec -i $(docker ps -q) /bin/bash -c 'cd /go/src/github.com/backend && nohup ./bee run > debug.log 2>&1 &; exit'
+docker exec -i $(docker ps -q) /bin/bash -c 'cd /go/src/github.com/backend && ./run.sh && exit'
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+nohup ./bee run > debug.log 2>&1 &
