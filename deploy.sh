87cf9b19cdf7b2a9c8ca2c908964cd8ffb58eeb1
leungyukshing <jacky14.liang@gmail.com>
2020-02-12 18:35:54 +0800

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ docker rmi -f test:latest
 docker build -t test -f Dockerfile .
 docker run --rm -it -d -p 8080:8080 --net=host test
 docker cp /root/bee $(docker ps -q):/go/src/github.com/backend
-docker exec -it $(docker ps -q) /bin/bash -c 'cd /go/src/github.com/backend && ./bee run'
+docker exec -i $(docker ps -q) /bin/bash -c 'cd /go/src/github.com/backend && ./bee run'
