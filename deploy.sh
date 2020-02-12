c7b325ef8acf25d47da0966b79cd8c3271b2b2b9
leungyukshing <jacky14.liang@gmail.com>
2020-02-12 17:03:22 +0800

    feat: use bee to start service

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 docker stop $(docker ps -aq)
 docker rmi -f test:latest
 docker build -t test -f Dockerfile .
-docker run --rm -it -d -p 8080:8080 --net=host test /bin/bash -c 'go run /go/src/github.com/backend/main.go'
+docker run --rm -it -d -p 8080:8080 --net=host test
+docker cp /root/bee $(docker ps -q):/go/src/github.com/backend
+docker exec -it $(docker ps -q) /bin/bash -c 'cd /go/src/github.com/backend && bee run'
