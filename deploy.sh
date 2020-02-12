2c458b0d6be011f89f611f1e2d943c65f38eda53
leungyukshing <jacky14.liang@gmail.com>
2020-02-12 21:19:02 +0800

    use go run

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 docker stop $(docker ps -aq)
 docker rmi -f test:latest
 docker build -t test -f Dockerfile .
-docker run --rm -it -d -p 8080:8080 --net=host test
-docker cp /root/bee $(docker ps -q):/go/src/github.com/backend
-docker exec -i $(docker ps -q) /bin/bash -c 'cd /go/src/github.com/backend && chmod +x ./run.sh && ./run.sh && exit'
+docker run --rm -it -d -p 8080:8080 --net=host test /bin/bash -c 'go run /go/src/github.com/backend/main.go'
